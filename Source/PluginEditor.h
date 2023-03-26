65e6d371b8358521af09beadb65fff60ace43f20
Mordorkonan <konanmordormordor@gmail.com>
2023-03-26 11:41:05 +0300

    Initial Commit

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class PFMCPP_Project10AudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    PFMCPP_Project10AudioProcessorEditor (PFMCPP_Project10AudioProcessor&);
+    ~PFMCPP_Project10AudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    PFMCPP_Project10AudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PFMCPP_Project10AudioProcessorEditor)
+};
